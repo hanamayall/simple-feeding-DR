27304f1029b5e338fe79bbdc08f07cf80155bf17
hanamayall <hana@hanachan.co.uk>
2022-01-21 11:33:03 +0000

    edit setup.jl

---
+++ setup.jl
@@ -0,0 +1,16 @@
+##### Packages #####
+
+## Import package manager
+import Pkg
+
+## Activate project
+Pkg.activate(".")
+
+## Add necessary packages to project
+Pkg.add("Plots")
+
+## check package status
+Pkg.status()
+
+## load packages
+using Plots
