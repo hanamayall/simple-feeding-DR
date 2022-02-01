0eea1b118604a6d0acee94bcdd3e79bc8d82bee6
hanamayall <hana@hanachan.co.uk>
2022-02-01 14:49:29 +0000

    update

--- biological_rates.jl
+++ biological_rates.jl
@@ -3,44 +3,6 @@
 
 ##### Parameters
 # Create a tuple containing the parameters required to calculate the mass/ temperature dependent biological rates
-param = (
-    # Intercepts
-    I_r = -15.68,
-    I_y = -9.66,
-    I_B0 = 3.44,
-    I_x = -16.54,
-    I_αm = -1.81,
-    I_thm = 1.92,
-    I_thT = 0.5,
-
-    # Resource slopes
-    si_K = 0.28,
-    si_r = -0.25,
-    si_y = 0.45,
-    si_B0 = 0.2,
-    si_x = -0.31,
-
-    # Consumer slopes (consumption)
-    sj_y = -0.47,
-    sj_B0 = 0.33,
-
-    # Ratio slopes (attack and handling)
-    s1_αm = 0.39,
-    s1_thm = -0.48,
-    s1_thT = -0.055,
-
-    # Quadratic ratio slopes (attack and handling)
-    s2_αm = -0.017,
-    s2_thm = 0.0256,
-    s2_thT = 0.0013,
-    
-    # Activation energies
-    Ea_K = 0.71,
-    Ea_r = -0.84,
-    Ea_y = -0.26,
-    Ea_B0 = 0.12,
-    Ea_x = -0.69
-)
 
 typeof(param)
 
@@ -77,9 +39,9 @@ end
 
 ## Carrying capacity - K 
 # the intercept I is varied to investigate the effects of enrichment
-function carryingcapacity_BA(param, M, T)
+function carryingcapacity_BA(;I_K, param, M, T)
     # extract parameter values from param
-    I = param.I_K
+    I = I_K
     si = param.si_K
     Ea = param.Ea_K
     # calculate terms in carrying capacity equation
